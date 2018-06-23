fcd10723c2fb0903c20b14d936ea7acc7f347375
Lorica7 <38511443+Lorica7@users.noreply.github.com>
2018-06-22 21:47:08 -0400

    search and buttons working

--- logic.js
+++ logic.js
@@ -5,23 +5,29 @@
     var animalArray = ["zebra", "dogs", "pigs", "sloth", "guinea-pig", "platypus", "kangaroo", "elephant", "chipmunk"];
  
      var animalContainer = document.getElementById("animal-info");
-    var searchReq=  $(this).val("searchReq");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchReq + "&api_key=P1UxBlMCbh1oybrMn1pVZvc7jexNd7sE&limit=5"
- 
-  
-   
+    
    
     button.addEventListener("click", function () {
+        event.preventDefault();
+        let searchReq = document.getElementById("user-input").value;
+        let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchReq + "&api_key=P1UxBlMCbh1oybrMn1pVZvc7jexNd7sE&limit=10";
+        
         $.ajax({
             url: queryURL,
             method: "GET"
           }).then(function(data) {
         { console.log("success got data", data); };
+        console.log(searchReq);
+        animalArray.push(searchReq);
+        $("#buttons-holder").empty();
+        renderButtons();
+        console.log(animalArray);
     });
 });
 
 
     function renderButtons (){
+        
         for (i = 0; i< animalArray.length; i ++){
             var btn = $("<button>");
             btn.addClass("animal-btn");
