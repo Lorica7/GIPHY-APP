09b5bca43c0a56a1b6d863f0f4b30842167e23d3
Lorica7 <38511443+Lorica7@users.noreply.github.com>
2018-06-23 19:32:02 -0400

    got images to clear and render on click

--- logic.js
+++ logic.js
@@ -1,45 +1,74 @@
 
-   
-    var button = document.getElementById("search");
+//declare variables
+var giphySearchButton = document.getElementById("search");
+var animalImages = document.getElementsByTagName("img");
+var animalArray = ["zebra", "dogs", "pigs", "sloth", "guinea-pig", "platypus", "kangaroo", "elephant", "chipmunk"];
 
-    var animalArray = ["zebra", "dogs", "pigs", "sloth", "guinea-pig", "platypus", "kangaroo", "elephant", "chipmunk"];
- 
-     var animalContainer = document.getElementById("animal-info");
-     
-    
-    button.addEventListener("click", function () {
-        
-        let searchReq = document.getElementById("user-input").value;
-        let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchReq + "&api_key=";
-        
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-          }).then(function(data) {
-        { console.log("success got data", data); };
-        console.log(searchReq);
-        animalArray.push(searchReq);
-        renderButtons();
-        console.log(animalArray);
+var animalContainer = document.getElementById("animal-images");
+var state = {
+    images: {
 
+    }
+}
+//add event listener to the search button.
+giphySearchButton.addEventListener("click", function () {
+    $("#animal-images").empty();
+    let searchReq = document.getElementById("user-input").value;
+    let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchReq + "";
+//making AJAX call
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (result) {
+        console.log("success got data", result);
+        if (result.data.length !== 0) {
+            state.images[searchReq] = [];
+        }
+     //looping through the array of results and accessing the different urls for still and moving gifs
+        for (let i = 0; i < result.data.length; i++) {
+            var movingImage = result.data[i].images.fixed_height.url;
+            var stillImage = result.data[i].images.fixed_height_still.url;
+            var imageObj = {movingImage: movingImage, stillImage: stillImage};
+            state.images[searchReq].push(imageObj)
+            var img = `<img class="giphyImages" data-searchterm="${searchReq}" id="${i}" src="${stillImage}"></img>`;
+            //var img = $("<img>")
+           // img.attr("src", src);
+           // append the images to the appointed div
+            $("#animal-images").append(img)
+            console.log("im here")
+        };
+    });
+//push the search term to the array
+    console.log(searchReq);
+    animalArray.push(searchReq);
+    //$("#buttons-holder").empty();
+//create a new button from the search entry, and put button in the div at top
+    var btn = $("<button>");
+    btn.addClass("animal-btn");
+    btn.attr("data-animal", searchReq);
+    btn.text(searchReq);
+    $("#button-holder").append(btn);
+
+    animalImages.addEventListener("click", function () {
+        $("#animal-images").empty();
 });
-});
-    function renderButtons (){
-        $("#buttons-holder").empty();
-        for (i = 0; i< animalArray.length; i ++){
-            var btn = $("<button>");
-            btn.addClass("animal-btn");
-            btn.attr("data-animal", animalArray[i]);
-            btn.text(animalArray[i]);
-            $("#button-holder").append(btn);
+// so you will need the 3 data-attributes, one for the still image, one for the animated and one to keep track of whether it is animated or still (a state indicator)
+// the gif will start wih the animated one and
+// just like the button an on click event that when the functions runs it will change 
+// use what is in the data attribute to swap between animated and still
+// make sure to use google if u get stuck
+
+//need to specify state
+
+// this function renders the buttons that already exists when the page loads
+function renderButtons() {
+
+    for (let i = 0; i < animalArray.length; i++) {
+        var btn = $("<button>");
+        btn.addClass("animal-btn");
+        btn.attr("data-animal", animalArray[i]);
+        btn.text(animalArray[i]);
+        $("#button-holder").append(btn);
     };
-    };
-  renderButtons ();
-/*
-   
-            
-         animalContainter.insertAdjacentHTML('beforeend', htmlString);
-     };
-  
- 
-     
+};
+renderButtons();
