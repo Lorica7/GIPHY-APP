6d925114434908246dc53fede7cf8950eb4ff1e3
Lorica7 <38511443+Lorica7@users.noreply.github.com>
2018-07-13 14:42:56 -0400

    everything working, can change states

--- logic.js
+++ logic.js
@@ -1,12 +1,8 @@
-
 //declare variables
 var giphySearchButton = document.getElementById("search");
 var animalImages = document.getElementsByTagName("img");
 var animalArray = ["zebra", "dogs", "pigs", "sloth", "guinea-pig", "platypus", "kangaroo", "elephant", "chipmunk"];
-// MIght not need this variable var topButtons = document.getElementsByClassName("btn-large");
 var animalContainer = document.getElementById("animal-images");
-
-let imagesArr2 = []
 
 var resultData = {}
 //add event listener to the search button.
@@ -21,19 +17,15 @@ giphySearchButton.addEventListener("click", function () {
         url: queryURL,
         method: "GET"
     }).then(function (result) {
-
         console.log("success got data", result);
-       
 
-        // }
         //looping through the array of results and accessing the different urls for still and moving gifs
-
         for (let i = 0; i < result.data.length; i++) {
             var movingImage = result.data[i].images.fixed_height.url;
             var stillImage = result.data[i].images.fixed_height_still.url;
             var imageObj = { movingImage: movingImage, stillImage: stillImage };
 
-            var img = `<img class="giphyImages" data-searchterm="${searchReq}" id="${i}" src="${stillImage}"></img>`;
+            var img = `<img class="giphyImages" data-searchterm="${searchReq}" id="${i}" src="${stillImage}" state="still"></img>`;
             //  console.log('img?', img)
             $("#animal-images").append(img)
             console.log("im here")
@@ -41,51 +33,35 @@ giphySearchButton.addEventListener("click", function () {
             console.log(imagesArr);
         };
     });
-    
 
-    
-    $('body').on('click', '.giphyImages', function () {
+    $('body').on('click', '.giphyImages', function (event) {
         let i = parseInt($(this).attr('id'));
-        console.log(i);
-        console.log(imagesArr)
-        console.log('clicking these animals')
-        console.log('changing url?', imagesArr[i].movingImage)
-        $(this).attr("src", imagesArr[i].movingImage);
-
+        let pictureSource = $(this).attr('src');
+        let pictureState = $(this).attr('state');
+        let movingLink = imagesArr[i].movingImage;
+        let stillLink = imagesArr[i].stillImage;
+        if (pictureState == 'still') {
+            event.target.setAttribute('src', imagesArr[i].movingImage);
+            event.target.setAttribute('state', 'moving');
+        } else if (pictureState == 'moving') {
+            event.target.setAttribute('src', imagesArr[i].stillImage);
+            event.target.setAttribute('state', 'still');
+        }
     });
 
-
-
-    //push the search term to the array
-    console.log(searchReq);
-
+    //push the search term to the array, unless it already exists
     if (animalArray.indexOf(searchReq) === (-1)) {
-    animalArray.push(searchReq);
-    //$("#buttons-holder").empty();
-    //create a new button from the search entry, and put button in the div at top
-    var btn = $("<button>");
-    btn.addClass("btn-large");
-    btn.attr("data-animal", searchReq);
-    btn.text(searchReq);
-    $("#button-holder").append(btn);
-} else {
-    console.log("This button already exists.")
-};
+        animalArray.push(searchReq);
+        var btn = $('<button>');
+        btn.addClass('btn-large');
+        btn.attr('data-animal', searchReq);
+        btn.text(searchReq);
+        $("#button-holder").append(btn);
+    } else {
+        console.log("This button already exists.")
+    };
 });
-// put event click for images below --->
 
-
-//tried to create a click event to change the image source on click
-
-
-
-// so you will need the 3 data-attributes, one for the still image, one for the animated and one to keep track of whether it is animated or still (a state indicator)
-// the gif will start wih the animated one and
-// just like the button an on click event that when the functions runs it will change 
-// use what is in the data attribute to swap between animated and still
-
-
-//need to specify state
 
 // this function renders the buttons that already exists when the page loads
 function renderButtons() {
@@ -103,34 +79,38 @@ renderButtons();
 $(".btn-large").click(function (event) {
     $("#animal-images").empty();
     let buttonReq = event.target.textContent;
-imagesArr2 = [];
-    console.log(buttonReq);
+    imagesArr2 = [];
     let searchURL = "https://api.giphy.com/v1/gifs/search?q=" + buttonReq + "&api_key=P1UxBlMCbh1oybrMn1pVZvc7jexNd7sE&limit=5";
     //making AJAX call
     $.ajax({
         url: searchURL,
         method: "GET"
     }).then(function (result) {
-        // if (result.data.length !== 0) {
-        //      images[buttonReq] = [];
-        //  }
+
         for (let i = 0; i < result.data.length; i++) {
             var movingImage = result.data[i].images.fixed_height.url;
             var stillImage = result.data[i].images.fixed_height_still.url;
             var imageObj = { movingImage: movingImage, stillImage: stillImage };
             imagesArr2.push(imageObj)
-            var img = `<img class="giphyImagesSet2" data-searchterm="${buttonReq}" id="${i}" src="${stillImage}"></img>`;
+            var img = `<img class="giphyImagesSet2" data-searchterm="${buttonReq}" id="${i}" src="${stillImage}" state="still"></img>`;
             $("#animal-images").append(img);
         };
+        
 
-        $('body').on('click', '.giphyImagesSet2', function () {
-            let ii = parseInt($(this).attr('id'));
-            console.log(ii);
-            console.log(imagesArr2);
-            console.log('clicking these animals')
-            console.log('changing url?', imagesArr2[ii].movingImage)
-            $(this).attr("src", imagesArr2[ii].movingImage);
-    
+        $('body').on('click', '.giphyImagesSet2', function (event) {
+            let i = parseInt($(this).attr('id'));
+            let pictureSource = $(this).attr('src');
+            let pictureState = $(this).attr('state');
+            let movingLink = imagesArr2[i].movingImage;
+            let stillLink = imagesArr2[i].stillImage;
+            console.log("CLICKING")
+            if (pictureState == 'still') {
+                event.target.setAttribute('src', imagesArr2[i].movingImage);
+                event.target.setAttribute('state', 'moving');
+            } else if (pictureState == 'moving') {
+                event.target.setAttribute('src', imagesArr2[i].stillImage);
+                event.target.setAttribute('state', 'still');
+            }
         });
     });
 });
